a274a38d66534a96ff4311164dc46a5616416047
FP <fp@pop-os.localdomain>
2021-08-06 10:11:50 +1200

    Wyre API almost solved

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: false,
+  env: {
+    WYRE_API_KEY: "AK-GRAHP83U-Q9QPPVLA-LPNMJARY-CPB8DM3E",
+    WYRE_SECRET_KEY: "SK-TRBTXJX7-TYQ9PMG9-UTY4C6HL-L39R83UF"
+  },
 }
--- pages/api/wyre.ts
+++ pages/api/wyre.ts
@@ -2,26 +2,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import CryptoJS from 'crypto-js';
 import nc from "next-connect";
+const axios = require('axios').default;
 
 const app = nc();
-
 
 // Example is assuming use of Express and Axios
 // Make sure you are using the proper API keys for the correct environment (test, prod).
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-        const axios = require('axios');
         const testUrl = "https://api.testwyre.com";
         const productionUrl = "https://api.sendwyre.com";
         const WYRE_TEST_API_KEY = process.env.WYRE_API_KEY;
         const YOUR_SECRET_KEY = process.env.WYRE_SECRET_KEY;
+        const accountId = "AC_7RHCPVJVVEQ";
 
         const signature = (url, data) => {
             const dataToBeSigned = url + data;
-            console.log(dataToBeSigned);
-            
-            
             const token = CryptoJS.enc.Hex.stringify(CryptoJS.HmacSHA256(dataToBeSigned.toString(CryptoJS.enc.Utf8), YOUR_SECRET_KEY));
-            //const token = "https://pay.testwyre.com/purchase?accountId=AC_7RHCPVJVVEQ&country=US&failureRedirectUrl=https%3A%2F%2Fwww.sendwyre.com&amount=10&redirectUrl=https%3A%2F%2Fwww.sendeyre.com&utm_campaign=AC_Y22RAQXEYV6&utm_medium=widget&paymentMethod=debit-card&reservation=EFX6DN9ECRPTPDJBEL6U&sourceCurrency=USD&utm_source=checkout"
             return token;
         }
 
@@ -30,7 +26,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             const url = `${testUrl}/v3/orders/reserve?timestamp=${timestamp}`;
             const headers = {};
             const body = {
-                referrerAccountId: "AC_7RHCPVJVVEQ"
+                referrerAccountId: accountId
             }
             const details = JSON.stringify(body);
             
